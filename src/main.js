70e92468b77df884516b8ff67b7bf77583641a09
Tons <1536750974@qq.com>
2024-05-20 22:39:19 +0800

    贪吃蛇1.0

--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
 import { createApp } from 'vue'
+//npm install pinia
+import { createPinia } from 'pinia'
+//npm i pinia-plugin-persistedstate持久化插件
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate"
+
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
+
+//createApp(App).mount('#app')
+const app=createApp(App)
+app.use(pinia)
+app.mount('#app')
